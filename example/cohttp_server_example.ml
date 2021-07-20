0e10c92ee0f13f67a6a8746ff5e2b9b5ec2e7950
Zach Baylin <zach@skolem.tech>
2021-07-19 23:05:56 -0400

    Example: refactor to use finalize_and_send

--- example/cohttp_server_example.ml
+++ example/cohttp_server_example.ml
@@ -2,7 +2,7 @@ open Lwt
 open Cohttp_lwt_unix
 
 module Handlers = struct
-  let root _ = Lwt.return (`OK, "Yay!", [])
+  let root _ = Lwt.return (`OK, "Yay!")
 
   let sleep req trace transaction =
     let length =
@@ -23,23 +23,24 @@ module Handlers = struct
               ~type_:"Type" ~subtype:"Subtype" ~action:"Action" ()
           in
           Lwt_unix.sleep (length /. float sections) >|= fun () ->
-          Elastic_apm.Span.finalize span
+          let (_ : Elastic_apm.Span.result) =
+            Elastic_apm.Span.finalize_and_send span
+          in
+          ()
       )
     in
-    list |> Lwt_list.map_s (fun c -> c ()) >|= fun spans ->
-    (`OK, Printf.sprintf "Slept for %fs" length, spans)
+    list |> Lwt_list.map_s (fun c -> c ()) >|= fun _ ->
+    (`OK, Printf.sprintf "Slept for %fs" length)
 
-  let not_found _ = Lwt.return (`Not_found, "Not found", [])
+  let not_found _ = Lwt.return (`Not_found, "Not found")
 end
 
 let route req =
-  let open Elastic_apm.Message in
   let path = req |> Request.uri |> Uri.path in
   let trace = Elastic_apm.Trace.of_headers (req |> Request.headers) in
-  let tags = [ ("path", `String path) ] in
-  let (_, transaction) =
-    Elastic_apm.Transaction.make_transaction ~tags ~trace ~name:path
-      ~type_:"request" ()
+  let (trace, transaction) =
+    Elastic_apm.Transaction.make_transaction ~trace ~name:path ~type_:"request"
+      ()
   in
   ( match path with
   | "/" -> Handlers.root req
@@ -47,21 +48,21 @@ let route req =
   | _ -> Handlers.not_found req
   )
   >|= fun resp ->
-  let (status, body, spans) = resp in
-  let spans = spans |> List.map (fun span -> Span span) in
-  let transaction = Elastic_apm.Transaction.finalize transaction in
-  let msgs = [ Transaction transaction ] @ spans in
-  (status, body, msgs)
+  let (status, body) = resp in
+  let (_ : Elastic_apm.Transaction.result) =
+    Elastic_apm.Transaction.finalize_and_send transaction
+  in
+  (status, body)
 
 let server =
   let callback _ req _ =
-    route req >>= fun (status, body, msgs) ->
-    Elastic_apm.Apm.send msgs;
-    Server.respond_string ~status ~body ()
+    route req >>= fun (status, body) -> Server.respond_string ~status ~body ()
   in
   Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
 
 let () =
+  let log_src = Logs.Src.create "apm" in
+  Logs.Src.set_level log_src (Some Logs.Debug);
   let service_name = "test_service" in
   let secret_token = Sys.getenv "APM_SECRET_TOKEN" in
   let url = Sys.getenv "APM_URL" |> Uri.of_string in
