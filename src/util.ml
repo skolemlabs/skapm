6f9fcfd83bc8435ce29f3be2bb1f34ba5126cfdd
Zach Baylin <zach@skolem.tech>
2021-08-29 18:17:39 -0400

    Util: move wrap_call to util and bubble up errors

--- src/util.ml
+++ src/util.ml
@@ -4,3 +4,23 @@ let read_file path =
   Lwt_unix.openfile path [ Unix.O_RDONLY ] 0o640
   >|= Lwt_io.of_fd ~mode:Lwt_io.Input
   >>= Lwt_io.read
+
+let wrap_call
+    ?(context = Span.Context.empty)
+    ~name
+    ~type_
+    ~subtype
+    ~action
+    ~parent
+    (f : unit -> 'a) =
+  let span = Span.make_span ~name ~type_ ~subtype ~action ~parent ~context () in
+  match f () with
+  | v ->
+    let (_ : Span.result) = Span.finalize_and_send span in
+    v
+  | exception exn ->
+    let st = Printexc.get_raw_backtrace () in
+    let error = Error.make ~parent:(parent :> Error.parent) st exn in
+    Message_queue.push (Error.to_message_yojson error);
+    let (_ : Span.result) = Span.finalize_and_send span in
+    raise exn
